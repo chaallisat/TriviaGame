572756f9ce9a28db9cf8524a3f80e18b2e6188be
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-25 21:11:09 -0700

    html and js update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,10 @@
 
 //question variables
 
-const questions = ["Does Sora have a Nobody?", "Is Xemnas Anesm's Nobody?", "Is Sora a keyblade master?"]
+const questions = ["Does Sora have a Nobody?", "Is Xemnas Anesm's Nobody?", "Is Sora a keyblade master?"];
+let intervalId;
+let clockRunning = false;
+let time = 10;
 
 /* const question1 = "Does Sora have a Nobody?";
 const question2 = "Is Xemnas Anesm's Nobody?";
@@ -13,10 +16,12 @@ const question3 = "Is Sora a keyblade master?"; */
 //on click of start button, display questions and start timer
 $("#start-alert").on("click", function(event) {
 $("#start-alert").empty();
+
+$("#timer").append("00:10");
   
 for (i = 0; i < questions.length; i++) {
   $("#questions").append("<h2>" + questions[i] + "<h2>") 
-  $("#questions").append("<input id='q11' class='yes' type='radio' value='true'> True"); 
+  $("#questions").append("<input id='q11' class='yes' type='radio' value='true'> True  " + "<input id='q11' class='no' type='radio' value='false'> False") 
 }
 
 /* $(".questions").append("<h2>" + question1 + "<h2>")
